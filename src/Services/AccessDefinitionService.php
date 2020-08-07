133896a9c2e909df5821ca39c143542e4bc978d2
Tom Roar Furunes <tom.roar@halloverden.no>
2020-08-07 14:10:21 +0200

    ADD: AccessDefinitionService is now handling all canX methods

--- src/Services/AccessDefinitionService.php
+++ src/Services/AccessDefinitionService.php
@@ -1,60 +1,170 @@
 <?php
 
+
 namespace HalloVerden\Security\Services;
 
-use HalloVerden\Security\AccessDefinitions\AccessDefinedProperty;
-use HalloVerden\Security\Interfaces\AccessDefinableInterface;
-use HalloVerden\Security\Interfaces\AccessDefinitionInterface;
-use HalloVerden\Security\Interfaces\AccessDefinitionServiceInterface;
 
+use HalloVerden\Security\AccessDefinitions\Metadata\AccessDefinitionClassMetadata;
+use HalloVerden\Security\AccessDefinitions\Metadata\AccessDefinitionPropertyMetadata;
+use HalloVerden\Security\Interfaces\AccessDefinitionServiceInterface;
+use HalloVerden\Security\Interfaces\SecurityInterface;
+use HalloVerden\Security\Voters\OauthAuthorizationVoter;
+use Metadata\MetadataFactoryInterface;
+
+/**
+ * Class AccessDefinitionService
+ *
+ * @package HalloVerden\Security\Services
+ */
 class AccessDefinitionService implements AccessDefinitionServiceInterface {
 
   /**
-   * @var iterable
+   * @var MetadataFactoryInterface
    */
-  private $accessDefinitions;
+  private $metadataFactory;
 
   /**
-   * AccessDefinitionService constructor.
-   *
-   * @param iterable $accessDefinitions
+   * @var SecurityInterface
    */
-  public function __construct(iterable $accessDefinitions) {
-    $this->accessDefinitions = $accessDefinitions;
+  private $security;
+
+  /**
+   * @var bool
+   */
+  private $allowNoMetadata;
+
+  /**
+   * AccessDefinition2Service constructor.
+   *
+   * @param MetadataFactoryInterface $metadataFactory
+   * @param SecurityInterface        $security
+   * @param bool                     $allowNoMetadata
+   */
+  public function __construct(MetadataFactoryInterface $metadataFactory, SecurityInterface $security, bool $allowNoMetadata = true) {
+    $this->metadataFactory = $metadataFactory;
+    $this->security = $security;
+    $this->allowNoMetadata = $allowNoMetadata;
   }
 
   /**
    * @inheritDoc
    */
-  public function handleAccessDefinable(AccessDefinableInterface $accessDefinable): void {
-    $properties = $accessDefinable->getAccessDefinedProperties();
-    $data = [];
-
-    foreach ($properties as $key => $accessDefinitionValueType) {
-      /* @var $accessDefinitionValueType AccessDefinedProperty */
-      if (!class_exists($accessDefinitionValueType->getAccessDefinitionClass()) || !$accessDefinition = $this->getAccessDefinition($accessDefinitionValueType->getAccessDefinitionClass())) {
-        throw new \RuntimeException('AccessDefinition class ' . $accessDefinitionValueType->getAccessDefinitionClass() . ' not found.');
-      }
-
-      $data[$key] = $accessDefinition->filterData($accessDefinitionValueType);
+  public function canCreate(string $class): bool {
+    if (null === $metadata = $this->getMetadata($class)) {
+      return $this->allowNoMetadata;
     }
 
-    $accessDefinable->setAccessDefinedProperties(
-      $data
-    );
+    return $this->canHandle($class, $metadata->canCreateScopes, $metadata->canCreateRoles, null);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function canRead(string $class): bool {
+    if (null === $metadata = $this->getMetadata($class)) {
+      return $this->allowNoMetadata;
+    }
+
+    return $this->canHandle($class, $metadata->canReadScopes, $metadata->canReadRoles, null);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function canUpdate(string $class): bool {
+    if (null === $metadata = $this->getMetadata($class)) {
+      return $this->allowNoMetadata;
+    }
+
+    return $this->canHandle($class, $metadata->canUpdateScopes, $metadata->canUpdateRoles, null);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function canDelete(string $class): bool {
+    if (null === $metadata = $this->getMetadata($class)) {
+      return $this->allowNoMetadata;
+    }
+
+    return $this->canHandle($class, $metadata->canDeleteScopes, $metadata->canDeleteRoles, null);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function canReadProperty(string $class, string $property): bool {
+    if (null === $propertyMetadata = $this->getPropertyMetadata($class, $property)) {
+      return $this->allowNoMetadata;
+    }
+
+    return $this->canHandle($class, $propertyMetadata->readScopes, $propertyMetadata->readRoles, $propertyMetadata->readMethod);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function canWriteProperty(string $class, string $property): bool {
+    if (null === $propertyMetadata = $this->getPropertyMetadata($class, $property)) {
+      return $this->allowNoMetadata;
+    }
+
+    return $this->canHandle($class, $propertyMetadata->writeScopes, $propertyMetadata->writeRoles, $propertyMetadata->writeMethod);
   }
 
   /**
    * @param string $class
-   * @return AccessDefinitionInterface|null
+   * @param string $property
+   *
+   * @return AccessDefinitionPropertyMetadata|null
    */
-  private function getAccessDefinition(string $class): ?AccessDefinitionInterface {
-    foreach ($this->accessDefinitions as $accessDefinition) {
-      if ($accessDefinition instanceof $class) {
-        return $accessDefinition;
-      }
+  private function getPropertyMetadata(string $class, string $property): ?AccessDefinitionPropertyMetadata {
+    if (null === $metadata = $this->getMetadata($class)) {
+      return null;
     }
 
-    return null;
+    return $metadata->getPropertyMetadata($property);
   }
+
+  /**
+   * @param string $class
+   *
+   * @return AccessDefinitionClassMetadata|null
+   */
+  private function getMetadata(string $class): ?AccessDefinitionClassMetadata {
+    $metadata = $this->metadataFactory->getMetadataForClass($class);
+
+    if (!$metadata instanceof AccessDefinitionClassMetadata) {
+      return null;
+    }
+
+    return $metadata;
+  }
+
+  /**
+   * @param string      $class
+   * @param array|null  $scopes
+   * @param array|null  $roles
+   * @param string|null $method
+   *
+   * @return bool
+   */
+  private function canHandle(string $class, ?array $scopes, ?array $roles, ?string $method): bool {
+    // If a token exists with any of the given scopes, access is granted
+    if ($scopes !== null && $this->security->isGranted(OauthAuthorizationVoter::OAUTH_SCOPE, $scopes)) {
+      return true;
+    }
+
+    // Otherwise, either the correct role needs to be present, or a method needs to grant access
+    if ($roles !== null && $this->security->isGrantedEitherOf($roles)) {
+      return true;
+    }
+
+    if ($method !== null && $method($class, $scopes, $roles, $method)) {
+      return true;
+    }
+
+    return false;
+  }
+
 }
